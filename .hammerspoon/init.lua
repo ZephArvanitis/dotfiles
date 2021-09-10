c0e3757f3ec1f5da29a53a56fd543980fe32b76d
Zeph Arvanitis <zeph@rescale.com>
2021-09-10 10:36:48 -0700

    Update hammerspoon config
    Unfortunately my coup de grace – a shortcut moving a safari/chrome
    zendesk tab into my zendesk Chrome Desktop application – doesn't work
    and I'm not sure why. But it's a starting point at least

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -4,6 +4,71 @@ hs.hotkey.bind({"alt"}, "R", function()
   hs.reload()
 end)
 
+-- Move a zendesk ticket from a browser to the zendesk "application"
+
+-- Let's do some application-specific stuff
+local moveToZendeskBinding = nil
+function moveToZendesk()
+    local win = hs.window.focusedWindow()
+    local clipboard = hs.pasteboard.getContents()
+    hs.eventtap.keyStroke({"cmd"}, "L")
+    hs.eventtap.keyStroke({"cmd"}, "C")
+    hs.eventtap.keyStroke({}, "ESCAPE")
+    hs.eventtap.keyStroke({}, "ESCAPE")
+    local url = hs.pasteboard.getContents()
+    local match = string.match(url, "rescale.zendesk.com/agent/tickets/%d+")
+    hs.pasteboard.setContents(clipboard)
+    if (match) then
+        hs.alert("It's a zendesk link!")
+        local ticketID = string.match(match, "(%d+)")
+        hs.application.launchOrFocus("Zendesk")
+        -- for reasons beyond my ken, sending this keyStroke doesn't work.
+        -- Is very sad :'(
+        hs.eventtap.keyStroke({"ctrl", "alt"}, "F", 500000)
+        hs.eventtap.keyStrokes(ticketID)
+        hs.eventtap.keyStroke({}, "ENTER")
+        hs.eventtap.keyStroke({}, "ENTER")
+    else
+        -- hs.alert("Non-zendesk link, nop nop nop")
+    end
+end
+
+-- attempting to debug keystroke sending, which works in some places but not others
+-- hs.hotkey.bind({"cmd"}, "J", nil, function()
+--     hs.alert('hello darkness')
+--     hs.eventtap.event.newKeyEvent(hs.keycodes.map.ctrl, true):post()
+--     hs.alert('cmd')
+--     hs.eventtap.event.newKeyEvent(hs.keycodes.map.alt, true):post()
+--     hs.alert('alt')
+--     hs.eventtap.event.newKeyEvent("f", true):post()
+--     hs.alert('u')
+--     hs.eventtap.event.newKeyEvent("f", false):post()
+--     hs.alert('u')
+--     hs.eventtap.event.newKeyEvent(hs.keycodes.map.alt, false):post()
+--     hs.alert('alt')
+--     hs.eventtap.event.newKeyEvent(hs.keycodes.map.ctrl, false):post()
+--     hs.alert('cmd')
+--     -- hs.eventtap.keyStroke({"alt", "cmd"}, "F")
+-- end)
+
+function applicationWatcher(appName, eventType, appObject)
+    if (appName == "Safari" or appName == "Google Chrome") then
+        if (eventType == hs.application.watcher.activated) then
+            if (moveToZendeskBinding ~= nil) then
+                moveToZendeskBinding:enable()
+            else
+                moveToZendeskBinding = hs.hotkey.bind({"alt", "shift"}, "Z", moveToZendesk)
+            end
+        elseif (eventType == hs.application.watcher.deactivated) then
+            if (moveToZendeskBinding ~= nil) then
+                moveToZendeskBinding:disable()
+            end
+        end
+    end
+end
+local appWatcher = hs.application.watcher.new(applicationWatcher)
+appWatcher:start()
+
 -- Moving windows around
 hs.hotkey.bind({"alt"}, "A", function()
   local win = hs.window.focusedWindow()
@@ -44,6 +109,14 @@ hs.hotkey.bind({"alt"}, "X", function()
   win:setFrame(f)
 end)
 
+-- Send keystrokes
+-- function keyStrokes(str)
+--   return function()
+--       hs.eventtap.keyStrokes(str)
+--   end
+-- end
+-- hs.hotkey.bind({"alt", "cmd"}, "L", keyStrokes("console.log("))
+
 -- Focus applications
 hs.hotkey.bind({"alt"}, "T", function()
   hs.application.launchOrFocus("iTerm")
@@ -75,4 +148,16 @@ end)
 
 hs.hotkey.bind({"alt"}, "W", function()
   hs.application.launchOrFocus("/Applications/TiddlyDesktop.app")
+end)
+
+hs.hotkey.bind({"alt"}, "N", function()
+  hs.application.launchOrFocus("Asana")
+end)
+
+hs.hotkey.bind({"alt"}, "V", function()
+  hs.application.launchOrFocus("DCV Viewer")
+end)
+
+hs.hotkey.bind({"alt"}, "1", function()
+  hs.application.launchOrFocus("1Password 7")
 end)
